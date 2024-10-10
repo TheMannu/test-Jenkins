41713d02cc090718f30f4d4771bae063072e3cf9
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2024-10-10 16:15:27 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ class output{
     public static void main(String args[]) {
         // Print Hello World to the console
         System.out.println("Hello, World! This is Hello Ashwani Kumar, How can I help you");
+        System.out.println("Testing Poll SCM triger");
     }
 }
