172f944d69dd5007a0e1b507da00f7eb84a98918
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2024-10-10 15:04:16 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class print{
+public class output{
     public static void main(String[] args) {
         // Print Hello World to the console
         System.out.println("Hello, World! This is Hello Ashwani Kumar, How can I help you");
