f4dc8f6b8ad850661b981d3d4aea69bded78985f
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2024-10-10 14:54:15 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class print{
+    public static void main(String[] args) {
+        // Print Hello World to the console
+        System.out.println("Hello, World! This is Hello Ashwani Kumar, How can I help you");
+    }
+}
